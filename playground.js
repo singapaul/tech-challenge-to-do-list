a64f4814bf0ccf8e51fcafaa3cdf6f2aec31df95
Paul <paulmichaelhardman@gmail.com>
2022-03-01 20:44:30 +0000

    new branch

---
+++ playground.js
@@ -0,0 +1,12 @@
+{
+  /* <form onsubmit="toDoItem">
+  <input type="text" name="fname">
+    <input type="submit" value="Submit">
+  </form> */
+}
+
+
+const toDoItem = () => {
+  alert("steps in the right diretion")
+
+}
--- script.js
+++ script.js
@@ -9,27 +9,34 @@ const toDoListContents = document.querySelector(".toDoContents")
 const toDoListFooter = document.querySelector(".toDoFunctions")
 const resetButton = document.querySelector(".resetButton");
 const submitTaskButton = document.querySelector(".submitButton");
-
-// delete button to appear in footer later
-// const deleteButton =
+const inputFieldTAsk = document.querySelector(".inputFieldTask");
 
 
+// make field invisible and not in html:
+inputFieldTAsk.style.visibility = "hidden";
+inputFieldTAsk.style.display = "none";
+submitTaskButton.style.visibility = "hidden";
+submitTaskButton.style.display = "none";
 
+
+let textString = ""
 // adding a keydown function
 // So when I press enter I want that box and form to submit and a new one to appear
 
-const addListItem = () => {
-  console.log(InputFieldBox.value);
+const addListItem = (evnt) => {
+  const Task = inputFieldTAsk.value;
+  //  alert(InputFieldBox.innerHTML)
+
   toDoListContents.innerHTML += `<div class=toDoItem>
   <input type="checkbox">
-  <p>Input Text</p>
+  <p>${Task}</p>
   <button type="button">@</button>
   </div>`
+  inputFieldTAsk.value = "";
 }
 
 // Get element value
 const whatIsYourNameFunction = (evnt) => {
-  const arrayOutput = [];
   if (evnt.keyCode === 13) {
     evnt.preventDefault();
     console.log(nameInputField.value)
@@ -37,33 +44,22 @@ const whatIsYourNameFunction = (evnt) => {
     // now we want to 1) update our HTML 2) hides the static text 3) change our input box to a task input box
     greetingTag.innerHTML = `Welcome back ${userName}!`
     staticText.innerHTML = "Let’s enter the tasks we need to complete for this upcoming week. Just use the input below to create a todo.";
-    bothInputs.innerHTML = `<div>
-    <input class=inputField type="text" placeholder="what are we doing today?"></input> <button class="submitButton">+</button></div>`
-    toDoListContents.innerHTML = `
-    <div class=allTasks><p>Tasks</p>
-    <div class=completedAllTasks>
-    <p class=placeholder>All tasks have been completed. Great job! - I will implement this later using a function that counts the number of children in the the todolist - if it is zero this will appear</p>
-    </div>
-    </div>
-    `;
-    // There must be a better way to write this?
-    const submitTaskButton = document.querySelector(".submitButton");
-    arrayOutput.push(submitTaskButton);
-
-    const placeholderText = document.querySelector(".placeholder");
-    arrayOutput.push(placeholderText);
-
-    const InputFieldBox = document.querySelector(".inputField");
-    arrayOutput.push(InputFieldBox);
-
-    submitTaskButton.addEventListener("click", addListItem);
-
-
+    // The element should now remove itself
+    // We should also be changing the class on the to do list inputs so that it appears when previusly hidden
+    nameInputField.remove();
+    inputFieldTAsk.style.visibility = "visible";
+    inputFieldTAsk.style.display = "inline-block";
+    submitTaskButton.style.visibility = "visible";
+    submitTaskButton.style.display = "contents";
   } else {
     console.log("no")
     return
   }
-  return arrayOutput;
+
+  // const submitTaskButton = document.querySelector(".submitButton");
+  // const InputFieldBox = document.querySelector(".inputField");
+  // console.log(InputFieldBox.innerHTML)
+  // submitTaskButton.addEventListener("click", addListItem(InputFieldBox.innerHTML))
 }
 
 
@@ -91,7 +87,6 @@ const resetHTML = () => {
 nameInputField.addEventListener("keyup", whatIsYourNameFunction);
 
 resetButton.addEventListener("click", resetHTML);
+submitTaskButton.addEventListener("click", addListItem);
 
 // next job is to make the input box have functionality 
-
-
