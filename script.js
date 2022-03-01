3b772bbe3c32dc7499a3ad388d111092837e6873
Paul <paulmichaelhardman@gmail.com>
2022-03-01 14:59:20 +0000

    added most DOM elements

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 console.log("hello world");
+// DOM elements 
+const greetingTag = document.querySelector(".greeting");
+const staticText = document.querySelector(".staticText");
+const inputField = document.querySelector(".inputField");
+const toDoListContents = document.querySelector(".toDoContents")
+const toDoListFooter = document.querySelector(".toDoFunctions")
+const resetButton = document.querySelector(".resetButton")
+// delete button to appear in footer later
+const deleteButton=
+
+console.log(greetingTag);
+console.log(staticText);
+console.log(inputField);
+console.log(toDoListContents);
+console.log(toDoListFooter);
+console.log(resetButton);
+
+
